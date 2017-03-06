82c76c99b45329f9b82fe9e6df038cd367ae04c4
gpmeadows <thatguy45@comcast.net>
2017-03-06 14:46:51 -0600

    Added RingBuffer

---
+++ RingBuffer.java
@@ -0,0 +1,14 @@
+//----------------------------------------
+// Name: Grayson Meadows
+// Class: CS 1233
+// Program: Buffer data structure as fifo
+// ---------------------------------------
+public class RingBuffer
+{
+  // Attribute
+  private int head; // head of the list(where to remove next element)
+  private int tail; // tail of the list (where to ass next element)
+  private iint size; // number of elements in the list
+
+  // Constructor
+}
