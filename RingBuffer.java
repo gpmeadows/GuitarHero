b5cd8b73176a981526880ed332008a7f57eccb4a
gpmeadows <thatguy45@comcast.net>
2017-03-08 14:11:37 -0600

    Added size function

--- RingBuffer.java
+++ RingBuffer.java
@@ -8,15 +8,21 @@ public class RingBuffer
   // Attribute
   private int head; // head of the list(where to remove next element)
   private int tail; // tail of the list (where to ass next element)
-  private int size; // number of elements in the list
-  private int[] buffer; // contains list of samples
+  private int count; // number of elements in the list
+  private double[] buffer; // contains list of samples
 
   // Default Constructor
   public RingBuffer()
   {
     head = 0;
     tail = 0;
-    size = 0;
-    
+    count = 0;
   }
+
+  public int size()  // returns number of elements in the buffer
+  {
+    return count; // (tail - head) % N
+  }
+
+  
 }
